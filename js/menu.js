d78bb725f27cfe61770468ce5fbfe39d9ffefb26
TheFollenBest <prototype.dp@gmail.com>
2022-01-05 00:51:45 +0700

    Add scss and mixins

--- js/menu.js
+++ js/menu.js
@@ -4,4 +4,5 @@ let navigation = document.getElementById('menu');
 
 button.addEventListener('click', function(){ 
     navigation.classList.toggle('display-block');
+    button.classList.toggle('header__menu-btn--pink');
 });
